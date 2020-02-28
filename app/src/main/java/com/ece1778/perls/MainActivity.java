16cfb05d4af8fef6afd33b36fe5f781574b1a153
Imtihan Ahmed <imtihan@ualberta.ca>
2020-02-28 18:13:03 -0500

    Project setup

---
+++ app/src/main/java/com/ece1778/perls/MainActivity.java
@@ -0,0 +1,14 @@
+package com.ece1778.perls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
