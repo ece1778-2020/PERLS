24e3996b673d5e21e433b4e8610b6b544971d65b
Imtihan Ahmed <imtihan@ualberta.ca>
2020-03-01 18:24:40 -0500

    Button functionality

---
+++ app/src/main/java/com/ece1778/perls/EmotionSelector.java
@@ -0,0 +1,14 @@
+package com.ece1778.perls;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EmotionSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emotion_selector);
+    }
+}
