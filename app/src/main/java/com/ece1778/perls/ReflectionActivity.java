ddd67c9576350ed37e61d8dbfe5c40c1bd3a379a
Imtihan Ahmed <imtihan@ualberta.ca>
2020-03-03 01:38:19 -0500

    added upload of reflection on complete

--- app/src/main/java/com/ece1778/perls/ReflectionActivity.java
+++ app/src/main/java/com/ece1778/perls/ReflectionActivity.java
@@ -1,38 +1,75 @@
 package com.ece1778.perls;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class ReflectionActivity extends AppCompatActivity {
 
     private TextView mTextView;
+    private EditText mReflectionText;
     private static final String EXERCISE_MESSAGE_ID = "exerciseId";
     private static final String TIMESTAMP_ID = "timestamp";
     private static final String EMOTION_ID = "emotion";
-    private String timestamp, uid, emotion;
+    private static final String REFLECTION_COLLECTION = "reflections";
+
+    private String timestamp, uid, emotion, reflection;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reflection);
 
+        mTextView = findViewById(R.id.textView);
+        mReflectionText = findViewById(R.id.reflection_editText);
         //get this from the emotion passed from emotion selector -> exercise activity
-        /*Intent intent = getIntent();
+        Intent intent = getIntent();
         emotion = intent.getStringExtra(EMOTION_ID);
         uid = intent.getStringExtra(EXERCISE_MESSAGE_ID);
-        timestamp = intent.getStringExtra(TIMESTAMP_ID);*/
-        mTextView = findViewById(R.id.textView);
-        mTextView.setText("You were feeling (put emotion here) before the exercise, how do you feel now?");
+        timestamp = intent.getStringExtra(TIMESTAMP_ID);
 
-
+        mTextView.setText("You were feeling " + emotion + " before the exercise, how do you feel now?");
 
     }
 
     public void goHome(View view) {
-        startActivity(new Intent(this, MainActivity.class));
+        Map<String, Object> reflection = new HashMap<>();
+        reflection.put("uid", uid);
+        reflection.put("prompt", mTextView.getText().toString());
+        reflection.put("response", mReflectionText.getText().toString());
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection(REFLECTION_COLLECTION).document(uid)
+                .set(reflection)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot reflection successfully written!");
+                        startActivity(new Intent(ReflectionActivity.this, MainActivity.class));
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error writing exercise document, try again", e);
+                        Toast.makeText(ReflectionActivity.this, "Failed upload, try again", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
