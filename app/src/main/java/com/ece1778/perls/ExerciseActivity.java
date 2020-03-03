c5087fe8f88adf40c1c2fef25374e0d58e5dac71
Ishimwem <ishimwe.merci@gmail.com>
2020-03-02 19:47:07 -0500

    modified Exercise activity UI

--- app/src/main/java/com/ece1778/perls/ExerciseActivity.java
+++ app/src/main/java/com/ece1778/perls/ExerciseActivity.java
@@ -9,36 +9,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class ExerciseActivity extends AppCompatActivity {
 
     private ExerciseViewModel mViewModel;
+    private ViewPagerAdapter mAdapter;
+    private ViewPager mViewPager;
+    private ImageView mNext;
+    private ImageView mPrevious;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise);
 
-        ViewPagerAdapter viewPagerAdapter =
-                new ViewPagerAdapter(this, getSupportFragmentManager());
-        ViewPager viewPager = findViewById(R.id.view_pager);
-        viewPager.setAdapter(viewPagerAdapter);
+        mAdapter = new ViewPagerAdapter(this, getSupportFragmentManager());
+        mViewPager = findViewById(R.id.view_pager);
+        mViewPager.setAdapter(mAdapter);
 
         mViewModel = new ViewModelProvider(this).get(ExerciseViewModel.class);
+        mNext = findViewById(R.id.next);
+        mPrevious = findViewById(R.id.previous);
+        //mPrevious.setVisibility(View.GONE);
+
+
     }
 
     public void cancelExercise(View view) {
-        /*Log.d("View Model Q1: ", mViewModel.getQ1().getValue().getAnswer());
-        Log.d("View Model Q2: ", mViewModel.getQ2().getValue().getAnswer());
-        Log.d("View Model Q3: ", mViewModel.getQ3().getValue().getAnswer());
-        Log.d("View Model Q4: ", mViewModel.getQ4().getValue().getAnswer());
-        Log.d("View Model Q5: ", mViewModel.getQ5().getValue().getAnswer());*/
-        //Log.d("cancelling...", "cancelling exercise");
-        //Log.d("Cancelling: ", mViewModel.getQ1().getValue().getAnswer());
-        //Toast.makeText(this, mViewModel.getQ2().getValue().getAnswer(), Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
 
+    }
+
+    public void goNext(View view) {
+        //Log.d("Exercise Activity", "getting into the goNext fcn");
+
+        mViewPager.setCurrentItem(mViewPager.getCurrentItem() + 1, true);
+    }
+
+    public void goPrevious(View view) {
+        //Log.d("Exercise Activity", "getting into the goPrevious fcn");
+        mViewPager.setCurrentItem(mViewPager.getCurrentItem() - 1, true);
     }
 }
